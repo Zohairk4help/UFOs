00ab60cd8d2951ac5f27495734e20a3822b1ea06
Zohairk4help <k_zohair@hotmail.com>
2021-12-27 11:40:22 -0500

    some editions

--- challenge/static/js/app.js
+++ challenge/static/js/app.js
@@ -48,7 +48,7 @@ function updateFilters() {
       delete filters[filteredID];
     }
 
-    console.log(filters)
+    
     // 6. Call function to apply all filters and rebuild the table
     filterTable();
   
@@ -61,12 +61,13 @@ function updateFilters() {
     let filteredData = tableData;
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
-    filteredData = Object.entries(filteredData).filter(([filters]) => {
-      console.log(`${JSON.stringify(key)} ${JSON.stringify(value)}`); 
+    Object.entries(filters).forEach(([key,value]) => {
+      filteredData = filteredData.filter(row => row[key] === value);
+      // console.log(`${JSON.stringify(key)} ${JSON.stringify(value)}`); 
     });
   
     // 10. Finally, rebuild the table using the filtered data
-    buildTable(filteredTable)
+    buildTable(filteredData)
   }
   
   // 2. Attach an event to listen for changes to each filter
